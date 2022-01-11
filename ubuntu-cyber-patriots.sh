019b346429b685415118c913a7a1e444dc9d5b4b
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-01-11 14:02:13 -0500

    Update ubuntu-cyber-patriots.sh

--- ubuntu-cyber-patriots.sh
+++ ubuntu-cyber-patriots.sh
@@ -180,6 +180,12 @@ cp sysctl_conf /etc/sysctl.conf
 (cat /etc/sysctl.conf ; echo "net.ipv4.tcp_syncookies = 1") > sysctl_conf
 cp sysctl_conf /etc/sysctl.conf
 
+(cat /etc/sysctl.conf ; echo "net.ipv6.conf.all.accept_ra = 0") > sysctl_conf
+cp sysctl_conf /etc/sysctl.conf
+
+(cat /etc/sysctl.conf ; echo "net.ipv6.conf.all.accept_ra = 0") > sysctl_conf
+cp sysctl_conf /etc/sysctl.conf
+
 rm sysctl_conf
 
 #Reloads the configuration issue
@@ -199,6 +205,10 @@ sysctl -w net.ipv4.route.flush=1
 
 sysctl -w net.ipv4.tcp_syncookies=1 
 sysctl -w net.ipv4.route.flush=1 
+
+sysctl -w net.ipv6.conf.all.accept_ra=0
+sysctl -w net.ipv6.conf.default.accept_ra=0
+sysctl -w net.ipv6.route.flush=1
 
 # Disables IP Forwarding
 grep -Els "^\s*net\.ipv4\.ip_forward\s*=\s*1" /etc/sysctl.conf /etc/sysctl.d/*.conf /usr/lib/sysctl.d/*.conf /run/sysctl.d/*.conf | while read -r filename; do sed -ri "s/^\s*(net\.ipv4\.ip_forward\s*)(=)(\s*\S+\b).*$/# *REMOVED* \1/" "$filename"; done; sysctl -w net.ipv4.ip_forward=0; sysctl -w net.ipv4.route.flush=1 
