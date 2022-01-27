3e3314abe06980603031bd7a98dfc52b557593a7
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-01-27 15:56:04 -0500

    Update and rename netstat to netstat.sh

--- netstat.sh
+++ netstat.sh
@@ -3,6 +3,9 @@
 echo "What would you like to do?"
 echo "(1) See which proccess is using a port (2) See which port a process is using"
 read -p n
-if [ n = "1"]
+if [ "$n" = "1"]
 then
-  echo "What port would you like to
+  echo "What port would you like to check?"
+  read -p port
+  netstat -ano -p tcp | grep $port
+  
