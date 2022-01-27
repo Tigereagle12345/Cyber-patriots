6fd89203a2e20e1b4e88cc65a4f4fa0394e9845d
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-01-27 16:30:03 -0500

    Update netstat.sh

--- netstat.sh
+++ netstat.sh
@@ -3,9 +3,36 @@
 echo "What would you like to do?"
 echo "(1) See which proccess is using a port (2) See which port a process is using"
 read -p n
-if [ "$n" = "1"]
+if [[ "$n" = "1"]]
 then
   echo "What port would you like to check?"
   read -p port
+  touch netstat_results
+  netstat -ano -p tcp >> netstat_results
+  head -2 netstat_results
+  rm netstat_results
   netstat -ano -p tcp | grep $port
+  echo "Please input the process ID"
+  read -r id
+  ps -p $id
+  echo "Do you wish to see the parent process of this process?"
+  read -p yn
+  if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]
+  then
+    while [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]
+    do
+      echo "Please input the process ID"
+      read -r id
+      ps -p $id
+      echo "Do you wish to see the parent process of this process?"
+      read -p yn
+    done
+  fi
+  
+elif [[ "$n" = "2"]]
+then
+  echo "hi"
+fi
+  
+  
   
