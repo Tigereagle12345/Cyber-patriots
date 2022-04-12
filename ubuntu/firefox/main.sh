e460f4f94c4d762698ee1332f25b8c5b40988d40
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-12 16:57:52 -0400

    Update main.sh

--- ubuntu/firefox/main.sh
+++ ubuntu/firefox/main.sh
@@ -6,6 +6,7 @@ grep Path= profiles.ini > profile_names
 sed 's/Path=//g' profile_names > profile_names
 while IFS= read -r profile
 do
-  cd ~/C
+  cd ~/Cyber-patriots/ubuntu/firefox/
+  cp user.js ~/.mozilla/firefox/$profile
 done < profile_names
 rm profile_names
