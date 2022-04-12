23cd0d5bea498d0059f77c721c1f7d41a0bac36e
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-12 16:59:41 -0400

    Update main.sh

--- ubuntu/firefox/main.sh
+++ ubuntu/firefox/main.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cd ~/.mozilla/firefox/
+cd ~/.mozilla/firefox/ || exit
 touch profile_names
 grep Path= profiles.ini > profile_names
-sed 's/Path=//g' profile_names > profile_names
+sed 's/Path=//g' profile_names > profiles.txt
 while IFS= read -r profile
 do
-  cd ~/Cyber-patriots/ubuntu/firefox/
-  cp user.js ~/.mozilla/firefox/$profile
-done < profile_names
+  cd ~/Cyber-patriots/ubuntu/firefox/ || return
+  cp user.js ~/.mozilla/firefox/"$profile"
+done < profiles.txt
 rm profile_names
+rm profiles.txt
