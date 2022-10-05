a6078c026caeecc8aa81daaac74ae65dc3aa4f42
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-05 15:23:15 -0400

    Update search.sh

--- ubuntu/extraCMDs/search.sh
+++ ubuntu/extraCMDs/search.sh
@@ -1,1 +1,20 @@
 #!/bin/bash/
+if [["$1"=="-u"]]
+then
+dir = "/home/'$2'/"
+
+elif [["$1"=="-u"]]
+then
+dir = "$2"
+
+else
+echo "Error: Invalid Input" && exit 1
+fi
+
+if [["$3"=="*"]]
+then
+ext = "*"
+else
+ext = "*'$3'"
+
+find "$dir" -type f -name "'$ext'"
