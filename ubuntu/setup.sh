8a5617296b680b117b4de67dd0a769e548b83467
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-05 17:50:53 -0400

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash/
 
 alias cypat-run="cd ~ && bash ./Cyber-patriots/ubuntu/main/ubuntu.sh"
-alias cypat-
+alias cypat-aide="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/aide.sh"
+alias cypat-clamav="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/clamav.sh"
+alias cypat-search="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/search.sh"
+alias cypat-man="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/man.sh"
