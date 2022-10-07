98f3eaf0b7eef357bf5244785b7b9422448fb0b3
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-07 18:30:36 -0400

    Update and rename setup.bat to setup.py

---
+++ windows10/setup.py
@@ -0,0 +1,8 @@
+import os
+
+# Define global variables
+yn_list = ["Yes", "YES", "yes", "y", "Y", "No", "NO", "no", "n", "N"]
+
+def alias():
+  yn = str(input("Configure Alias's to the style of Ubuntu Linux? Y/N: "
+
