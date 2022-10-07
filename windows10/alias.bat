83a3d676292153f20a3bd2f410c8e74b7e32b702
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-07 18:51:12 -0400

    Create alias.bat

---
+++ windows10/alias.bat
@@ -0,0 +1,10 @@
+REM Config ls
+DOSKEY ls=dir /B
+REM Config nano
+DOSKEY nano=notepad.exe $*
+REM Config vi
+DOSKEY vi=notepad.exe $*
+REM Config vim
+DOSKEY vim=notepad.exe $*
+REM Config cat
+DOSKEY cat=type $*
