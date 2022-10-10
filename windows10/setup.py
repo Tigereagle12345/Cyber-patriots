fe8d76347820dcac6b548b53296d7b787a28946e
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-10 19:42:38 -0400

    Delete setup.py

--- windows10/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Define global variables
-yn_list = ["Yes", "YES", "yes", "y", "Y", "No", "NO", "no", "n", "N"]
-
-def alias():
-  yn = str(input("Configure Alias's to the style of Ubuntu Linux? Y/N: "
-
