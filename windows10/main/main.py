820996a25f3b1b41f69943ca23a377832035e653
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-10 20:31:24 -0400

    Update main.py

--- windows10/main/main.py
+++ windows10/main/main.py
@@ -18,6 +18,25 @@ def firefox():
 def banner():
   banner = "###################################################################################################"
   print(banner)
+  
+def firewall():
+  os.system("netsh advfirewall set allprofiles state on")
+  firewall_port("off", 21)
+  firewall_port("off", 23)
+  firewall_port("off", 139)
+  firewall_port("off", 445)
+  firewall_port("off", 1433)
+  firewall_port("off", 1434)
+  firewall_port("off", 3306)
+  firewall_port("on", 22)
+  
+def firewall_port(off_on, port):
+  if off_on == "on":
+    os.system(f'netsh advfirewall firewall add rule name="Allow Port {str(port)}" dir=in action=allow protocol=TCP remoteport={str(port)}')
+  elif off_on == "off":
+    os.system(f'netsh advfirewall firewall add rule name="Allow Port {str(port)}" dir=in action=block protocol=TCP remoteport={str(port)}')
+  else:
+    print("Error: Incorrect port rule!")
 
 banner()
 firefox()
