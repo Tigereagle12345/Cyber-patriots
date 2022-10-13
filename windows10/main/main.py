97e7cc9ec531dd52fd7654debafa5b6e8ddbf9fd
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-13 18:09:48 -0400

    Update main.py

--- windows10/main/main.py
+++ windows10/main/main.py
@@ -26,7 +26,7 @@ def firefox():
 def banner():
   banner = "###################################################################################################"
   print(banner)
-  
+
 def firewall():
   os.system("netsh advfirewall set allprofiles state on")
   firewall_port("off", 21)
@@ -37,7 +37,7 @@ def firewall():
   firewall_port("off", 1434)
   firewall_port("off", 3306)
   firewall_port("on", 22)
-  
+
 def firewall_port(off_on, port):
   if off_on == "on":
     os.system(f'netsh advfirewall firewall add rule name="Allow Port {str(port)}" dir=in action=allow protocol=TCP remoteport={str(port)}')
@@ -47,45 +47,53 @@ def firewall_port(off_on, port):
     print("Error: Incorrect port rule!")
 
 def user_management_add(yn_list):
-  t = True
   os.system("net user")
   yn = str(input("Do you want to add any users? Y/N "))
   if yn in yn_list["y"]:
-    while t == True:
-      username = str(input("Username to add: "))
-      os.system(f"net user {username} q^yRpNgbes2wM*xR /add")
-      user_management_add(yn_list)
+    username = str(input("Username to add: "))
+    os.system(f"net user {username} q^yRpNgbes2wM*xR /add")
+    user_management_add(yn_list)
       
-   elif yn in yn_list["n"]:
-      print("Ok, Moving on...")
-      t = False
-   
+  elif yn in yn_list["n"]:
+    print("Ok, Moving on...")
+  
   else:
-      print("Error: Non-Standard Input")
+    print("Error: Non-Standard Input")
 
 def user_management_delete(yn_list):
-  t = True
   os.system("net user")
   yn = str(input("Do you want to remove any users? Y/N "))
   if yn in yn_list["y"]:
-    while t == True:
-      username = str(input("User to remove: "))
-      os.system(f"net user {username} /delete")
-      user_management_delete(yn_list)
+    username = str(input("User to remove: "))
+    os.system(f"net user {username} /delete")
+    user_management_delete(yn_list)
       
   elif yn in yn_list["n"]:
-      print("Ok, Moving on...")
-      t = False
-      
-  else:
-      print("Error: Non-Standard Input")
+    print("Ok, Moving on...")
 
+  else:
+    print("Error: Non-Standard Input")
+
+def user_management_change(yn_list):
+  os.system("net user")
+  yn = str(input("Do you want to change any user passwords? Y/N "))
+  if yn in yn_list["y"]:
+    username = str(input("User to change password: "))
+    os.system(f"net user {username} {password}")
+    user_management_change(yn_list)
       
+  elif yn in yn_list["n"]:
+    print("Ok, Moving on...")
+
+  else:
+    print("Error: Non-Standard Input")
+
 def user_management(yn_list):
   user_management_add(yn_list)
   user_management_delete(yn_list)
-  #user_management_change(yn_list)
+  user_management_change(yn_list)
 
-user_management(yn_list)
 banner()
+user_management(yn_list)
+firewall()
 firefox()
