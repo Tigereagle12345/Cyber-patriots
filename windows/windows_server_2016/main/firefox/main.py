d6b1fc5b1befa3b0cf92b00ae17f14ffbd3ad40c
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-18 10:21:48 -0400

    Create main.py

---
+++ windows/windows_server_2016/main/firefox/main.py
@@ -0,0 +1,25 @@
+import os
+
+profiles = []
+
+os.system("cd ~")
+os.system("cd %AppData%/Mozilla/Firefox/")
+file = open("profiles.ini", "r")
+for line in file:
+    if "Path=" in line:
+        profile_path = line.replace("Path=", "")
+        profile_path = profile_path.replace("\n", "")
+        profiles.append(profile_path)
+file.close
+        
+for item in profiles:
+    os.system("cd ~")
+    os.system("cd Cyber-patriot/windows10/main/firefox/")
+    prefs = open("user.js", "r")
+    os.system("cd ~")
+    os.system("cd %AppData%/Mozilla/Firefox/")
+    users = open("user.js", "a")
+    for line in prefs:
+        users.write(line)
+    prefs.close
+    users.close
