6490016354b21ae8a239a2c19ee343789474896b
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-18 09:48:05 -0400

    Update alias.bat

--- windows/windows_server_2016/alias.bat
+++ windows/windows_server_2016/alias.bat
@@ -1,1 +1,17 @@
-
+@echo off
+REM Config ls
+doskey ls=dir /B
+REM Config nano
+doskey nano=notepad.exe $*
+REM Config vi
+doskey vi=notepad.exe $*
+REM Config vim
+doskey vim=notepad.exe $*
+REM Config cat
+doskey cat=type $*
+REM Config rm
+doskey rm=del $* && Command Completed Sucessfully!
+reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d %~dp0\alias.bat /f
+cls
+echo Microsoft Windows [Version 10.0.19043.2006]
+echo (c) Microsoft Corporation. All rights reserved.
