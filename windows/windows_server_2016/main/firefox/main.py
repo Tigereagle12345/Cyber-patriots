f35fb3d5cf29d28800e4d3467d0029176edb36fc
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-18 22:09:35 -0400

    Update main.py

--- windows/windows_server_2016/main/firefox/main.py
+++ windows/windows_server_2016/main/firefox/main.py
@@ -2,24 +2,24 @@ import os
 
 profiles = []
 
-os.system("cd ~")
-os.system("cd %AppData%/Mozilla/Firefox/")
-file = open("profiles.ini", "r")
+appdata = os.getenv("appdata")
+
+file = open(str(appdata) + r"\Mozilla\Firefox\profiles.ini", "r")
 for line in file:
-    if "Path=" in line:
-        profile_path = line.replace("Path=", "")
-        profile_path = profile_path.replace("\n", "")
-        profiles.append(profile_path)
-file.close
+	if "Path=" in line:
+		profile_path = line.replace("Path=", "")
+		profile_path = profile_path.replace(r"\n", "")
+		profiles.append(profile_path)
         
+file.close
+
 for item in profiles:
-    os.system("cd ~")
-    os.system("cd Cyber-patriot/windows10/main/firefox/")
-    prefs = open("user.js", "r")
-    os.system("cd ~")
-    os.system("cd %AppData%/Mozilla/Firefox/")
-    users = open("user.js", "a")
-    for line in prefs:
-        users.write(line)
-    prefs.close
-    users.close
+	os.system("cd ~")
+	os.system("cd Cyber-patriot/windows10/main/firefox/")
+	prefs = open("user.js", "r")
+	users = open(str(appdata) + r"\Mozilla\Firefox\user.js", "a")
+	for line in prefs:
+		users.write(line)
+        
+	prefs.close()
+	users.close()
