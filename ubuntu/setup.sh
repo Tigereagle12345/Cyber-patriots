75dcc6150f54eaffa6a5e1f0548abff626fa6e1d
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-27 13:50:24 +0000

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash/
 
-alias cypat-run="cd ~ && bash ./Cyber-patriots/ubuntu/main/ubuntu.sh"
-alias cypat-aide="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/aide.sh"
-alias cypat-clamav="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/clamav.sh"
-alias cypat-search="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/search.sh"
-alias cypat-man="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/man.sh"
-alias cypat-uid="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/giveUID.sh"
+home_dir = "$HOME" + "/.bashrc"
+
+echo -e '\nalias cypat-run="cd ~ && bash ./Cyber-patriots/ubuntu/main/ubuntu.sh"' >> "$home_dir"
+echo -e 'alias cypat-aide="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/aide.sh"' >> "$home_dir"
+echo -e 'alias cypat-clamav="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/clamav.sh"' >> "$home_dir"
+echo -e 'alias cypat-search="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/search.sh"' >> "$home_dir"
+echo -e 'alias cypat-man="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/man.sh"' >> "$home_dir"
+echo -e 'alias cypat-uid="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/giveUID.sh"' >> "$home_dir"
 echo "Done"
