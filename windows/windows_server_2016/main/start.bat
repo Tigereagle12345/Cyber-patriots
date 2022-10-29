e77d042064cba218fde50b25b17ceace595cae63
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-29 12:28:23 -0400

    Update start.bat

--- windows/windows_server_2016/main/start.bat
+++ windows/windows_server_2016/main/start.bat
@@ -1,2 +1,15 @@
 @echo off
-start main.py
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+        start main.py
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+    
+    pause >nul
