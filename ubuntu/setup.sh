d121569050f501c370e7333855af927233c04f7d
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-31 12:03:51 -0400

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -4,10 +4,10 @@ user="${SUDO_USER:-$USER}"
 
 home_dir="/home/$user/.bashrc"
 
-echo -e '\nalias cypat-run="cd ~ && bash ./Cyber-patriots/ubuntu/main/ubuntu.sh>
-echo -e '\nalias cypat-aide="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/>
-echo -e '\nalias cypat-clamav="cd ~ && bash ./Cyber-patriots/ubuntu/main/suppor>
-echo -e '\nalias cypat-search="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/s>
-echo -e '\nalias cypat-man="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/man.>
-echo -e '\nalias cypat-uid="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/give>
+echo -e '\nalias cypat-run="cd ~ && bash ./Cyber-patriots/ubuntu/main/ubuntu.sh"'
+echo -e '\nalias cypat-aide="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/aide.sh"'
+echo -e '\nalias cypat-clamav="cd ~ && bash ./Cyber-patriots/ubuntu/main/support/clamav.sh"'
+echo -e '\nalias cypat-search="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/search.sh"'
+echo -e '\nalias cypat-man="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/man.sh"'
+echo -e '\nalias cypat-uid="cd ~ && bash ./Cyber-patriots/ubuntu/extraCMDs/give.sh"'
 echo "Done"
