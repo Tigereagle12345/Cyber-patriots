42bc21dd379d5ad9db6239ce6663dfb53f1753ed
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-11-20 18:13:41 -0500

    Update ubuntu.sh

--- ubuntu/main/ubuntu.sh
+++ ubuntu/main/ubuntu.sh
@@ -983,6 +983,8 @@ name="${file_path%'Cyber-patriots'*}"
 
 mv "$name/Cyber-patriots/" "$home_dir"
 
+rm -r -f "/home/$users/Cyber-patriots/windows/"
+
 # Run Setup Script
 bash ./support/setup.sh
 
