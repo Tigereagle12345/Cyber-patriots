37d6dd6d696e5b508b4b132125dfa5b44d07287d
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-12-05 16:47:29 -0500

    Update main.py

--- ubuntu/main/firefox/local-settings/main.py
+++ ubuntu/main/firefox/local-settings/main.py
@@ -1,2 +1,14 @@
-import os
+import shutil
+import argparse
 
+my_parser = argparse.ArgumentParser(description="Sets the location for the Firefox system files.")
+my_parser.add_argument('location',
+                       metavar='location',
+                       type=str,
+                       help='Firefox system files.')
+args = my_parser.parse_args()
+location = args.location
+
+location = location+"defaults/pref/"
+
+shutil.copy("local-settings.js", location)
