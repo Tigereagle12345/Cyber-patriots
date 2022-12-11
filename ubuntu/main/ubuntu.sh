8505af69228c80e12a075f30953b5fcd2f2ecf45
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-12-10 20:19:32 -0500

    Fixed Group Removal

--- ubuntu/main/ubuntu.sh
+++ ubuntu/main/ubuntu.sh
@@ -613,28 +613,9 @@ then
         echo "Would you like to add anyone else?"
         read -r yn
       done
-    elif [[ "$add_delete" = "delete" ]] || [[ "$add_delete" = "Delete" ]]
-    then
-     echo "Who would like to remove?"
-     read -r delete
-     usermod -a -G "$group" "$delete"
-     echo "User $delete removed from $group"
-     echo "Would you like to remove anyone else?"
-     read -r yn
-     if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]
-     then 
-       while [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]
-       do
-         echo "Who would like to delete?"
-         read -r delete
-         usermod -a -G "$group" "$delete"
-         echo "User $delete removed from $group"
-         echo "Would you like to remove anyone else?"
-         read -r yn
-    fi
-  fi
+     fi 
   
-else
+ else
   if [[ "$add_delete" = "delete" ]] || [[ "$add_delete" = "Delete" ]]
   then
     echo "Who would like to remove?"
