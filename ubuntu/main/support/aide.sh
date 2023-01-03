19f9c991b1b45cbc0beaca8c0f65730ef6c62944
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2023-01-03 17:12:08 -0500

    Update aide.sh

--- ubuntu/main/support/aide.sh
+++ ubuntu/main/support/aide.sh
@@ -4,3 +4,5 @@ apt install aide aide-common
 aideinit
 mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
 prelink -ua
+
+echo -e "0 5 * * * /usr/bin/aide.wrapper --config /etc/aide/aide.conf --check" || crontab -u root -e
