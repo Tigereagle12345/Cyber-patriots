4be34943bb30ee8f369da9369f9a6d92edb5f5e8
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2023-01-03 17:09:54 -0500

    Update aide.sh

--- ubuntu/main/support/aide.sh
+++ ubuntu/main/support/aide.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/bash
 
+apt install aide aide-common
 aideinit
 mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
 prelink -ua
-
-(cat /etc/systemd/system/aidecheck.service ; echo " [Unit] 
-Description=Aide check every day at 5AM
-
- [Timer] 
-OnCalendar=*-*-* 05:00:00 Unit=aidecheck.service
-
- [Install] 
-WantedBy=multi-user.target") > aide_conf
-cp aide_conf /etc/systemd/system/aidecheck.service
-rm aide_conf
-
-chown root:root /etc/systemd/system/aidecheck.* 
-chmod 0644 /etc/systemd/system/aidecheck.* 
-systemctl daemon-reload 
-systemctl enable aidecheck.service 
-systemctl --now enable aidecheck.timer
